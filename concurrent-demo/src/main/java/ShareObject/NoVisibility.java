16cd89a48fbb2875865c32811d0efacba899e2e7
litw <2394562735@qq.com>
2016-10-08 19:10:10 +0800

    add novisibility

--- concurrent-demo/src/main/java/ShareObject/NoVisibility.java
+++ concurrent-demo/src/main/java/ShareObject/NoVisibility.java
@@ -4,5 +4,23 @@ package ShareObject;
  * Created by Jo on 10/8/16.
  */
 public class NoVisibility {
-	
+	private static boolean ready;
+	private static int number;
+
+	private static class ReaderThread extends Thread{
+		public void run(){
+			while(!ready){
+				Thread.yield();
+			}
+			System.out.println(number);
+		}
+	}
+	public static void main(String[] args){
+		for(int i = 0;i<100;i++) {
+			//由于重排序的缘故,执行顺序可能并不是按照预期执行
+			new ReaderThread().start();
+			number = 100;
+			ready = true;
+		}
+	}
 }
