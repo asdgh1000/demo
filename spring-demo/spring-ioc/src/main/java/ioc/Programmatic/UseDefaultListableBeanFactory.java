a69702fa9103b7c616812a592eba794f01361026
litw <2394562735@qq.com>
2016-10-09 17:25:12 +0800

    add UseDefaultListableBeanFactory

---
+++ spring-demo/spring-ioc/src/main/java/ioc/Programmatic/UseDefaultListableBeanFactory.java
@@ -0,0 +1,21 @@
+package ioc.Programmatic;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Created by Jo on 10/9/16.
+ */
+public class UseDefaultListableBeanFactory {
+	public void initXmlBeanDefinitionreader(String xmlFile) {
+		FileSystemXmlApplicationContext
+		ClassPathResource resource = new ClassPathResource(xmlFile);//包含了beanDefinition的抽象资源
+		//DefaultListableBeanFactory是很多容器的基本容器
+		DefaultListableBeanFactory defaultListableBeanFactory = new DefaultListableBeanFactory();  //BeanFactory
+		XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(defaultListableBeanFactory);//为BeanDefinition读取器设置BeanFactory
+		xmlBeanDefinitionReader.loadBeanDefinitions(resource);//载入BeanDefinition并通过回调配置给BeanFactory
+	}
+}
