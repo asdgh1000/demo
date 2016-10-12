eca9ff319a9cde3219381780ae3c106eaf6d0e81
litw <2394562735@qq.com>
2016-10-12 15:00:26 +0800

    add programmatic

--- spring-demo/spring-ioc/src/main/java/ioc/Programmatic/Solution.java
+++ spring-demo/spring-ioc/src/main/java/ioc/Programmatic/Solution.java
@@ -1,27 +1,59 @@
 package ioc.Programmatic;
 
+import com.sun.tools.hat.internal.util.ArraySorter;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * Created by Jo on 10/9/16.
  */
 public class Solution {
-	public int climbStairs(int n) {
-		if(n != 1) {
-			int n1 = 1;
-			int n2 = 2;
-			int a = n - 2;
-			while (a-- > 0) {
-				int tmp = n1;
-				n1 = n2;
-				n2 = n2 + tmp;
+    public String addStrings(String num1, String num2) {
+		if(num1.length()==1 && num2.length()==1){
+			return (num1.toCharArray()[0]-'0' + num2.toCharArray()[0]-'0')+"";
+		}
+		int x = num1.length()-num2.length();
+		String a2 = "";
+		String b2 = "";
+		if(x>0){
+			StringBuffer a1 = new StringBuffer();
+			for(int i=0;i<x;i++) {
+				a1.append("0");
 			}
-			return n2;
+			a2 = a1.toString()+num2;
+			b2 = num1;
 		}
 		else {
-			return 1;
+			StringBuffer b1 = new StringBuffer();
+			for(int i=0;i<-x;i++){
+				b1.append("0");
+			}
+			b2 = b1.toString()+num1;
+			a2 = num2;
 		}
-	}
+		char[] a = a2.toCharArray();
+		char[] b = b2.toCharArray();
+		StringBuffer z = new StringBuffer();
+		int[] sum = new int[a.length+1];
+		for(int i =a.length-1;i>=0;i--){
+			if(sum[i+1]+(a[i]-'0')+(b[i]-'0')>9){
+				sum[i+1]+=(a[i]-'0')+(b[i]-'0');
+				sum[i]++;
+				sum[i+1]=sum[i+1]%10;
+			}
+			else{
+				sum[i+1] = sum[i+1]+(a[i]-'0')+(b[i]-'0');
+			}
+		}
+		for(int f : sum){
+			z.append(f);
+		}
+		String back = z.toString().replaceFirst("^0*","");
+		return back;
+    }
 	public static void main(String[] args){
 		Solution solution = new Solution();
-		System.out.println(solution.climbStairs(10));
+		System.out.println(solution.addStrings("99", "9"));
 	}
 }
