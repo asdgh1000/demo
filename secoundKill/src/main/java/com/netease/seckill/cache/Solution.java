a30648c93cbd8f63ae122c254c7e47b0e0a5575b
litw <2394562735@qq.com>
2016-11-06 21:08:46 +0800

    nothing

---
+++ secoundKill/src/main/java/com/netease/seckill/cache/Solution.java
@@ -0,0 +1,16 @@
+package com.netease.seckill.cache;
+
+/**
+ * Created by Jo on 11/6/16.
+ */
+public class Solution {
+	public boolean isPowerOfFour(int num){
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Math.pow(4,15));
+		return false;
+	}
+	public static void main(String[] args){
+		Solution solution = new Solution();
+		System.out.println(solution.isPowerOfFour(16));
+	}
+}
